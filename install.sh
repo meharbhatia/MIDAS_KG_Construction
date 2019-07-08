ba2dd11911e57c2adc22f79dd53ab188ac55bfd4
meharbhatia <meharmailbox@gmail.com>
2019-07-08 17:40:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo python3 -m nltk.downloader averaged_perceptron_tagger
 sudo python3 -m nltk.downloader maxent_ne_chunker
 sudo python3 -m nltk.downloader words
 sudo pip3 install neuralcoref
-sudo pip3 install -U spacy==2.1.3
+sudo pip3 install -U spacy==2.1.0
 sudo python3 -m spacy download en
 sudo pip3 install sumy
 

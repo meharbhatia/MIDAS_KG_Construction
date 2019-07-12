ce27b0aa811ff3d16ff546e56db628079ef02868
ritwikmishra <ritwik.m07@gmail.com>
2019-07-12 18:53:46 +0530

    implemented spacyNER

--- spacyNER+rules.py
+++ spacyNER+rules.py
@@ -6,6 +6,12 @@ from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
 from nltk import sent_tokenize
 import csv
+import spacy
+from spacy import displacy
+from collections import Counter
+import en_core_web_sm
+
+nlp = en_core_web_sm.load()
 
 ex = 'BYD debuted its E-SEED GT concept car and Song Pro SUV alongside its all-new e-series models at the Shanghai International Automobile Industry Exhibition.'
 # ex = "John was very fast"
@@ -24,74 +30,92 @@ def preprocess(sent):
 	return sent
 
 def getTriplets(ex):
-	# print(ex)
+	print(ex)
+	ex = ex.replace('‘','\'').replace('’','\'').replace('“','\'').replace('”','\'')
 
 	sent = preprocess(ex)
-	# print("NORMAL POS TAGGING")
-	# print(sent)
-	# print("NER DEFAULT")
-	# print(nltk.ne_chunk(sent))
+	print("NORMAL POS TAGGING")
+	print(sent)
+	print("NER DEFAULT")
+	print(nltk.ne_chunk(sent))
 
 
 	triplets = []
 	nn = ()
 	ml = []
+
+	## NER is being performed here
+	doc = nlp(ex)
+	spacyml = [(X, X.ent_iob_, X.ent_type_) for X in doc]
+	print(len(sent), len(spacyml))
+	newml = []
+	pos_tags = [(i, i.tag_) for i in doc]
+	sent = pos_tags
+	print("Spacy POS tagging")
+	print(sent)
+	print(spacyml)
 	nstr = ""
 	k=0
-	# All the consecutive NN pos tags are merged in this loop
-	while k<len(sent):
-		if ("NN" in sent[k][1]):
-			if (k+1 >= len(sent)) or not (
-				("\'s" in sent[k][0] or "’s" in sent[k][0] or sent[k+1][0] == '’' ) #if the noun is containing a possesion like John’s, Mary’s, Teachers’
-				and
-				(len(sent[k][0]) > 1 ) ): #so that it doesn't filter the names like O’Reily and not John’s
-				nstr = nstr + sent[k][0] + " "
-			else:
-				if ("\'s" in sent[k][0] or "’s" in sent[k][0]):
-					sent[k] = (sent[k][0], 'POSS')
-				elif (sent[k+1][0] == "\'s" or sent[k+1][0] == "’s"):
-					sent[k] = (sent[k][0], 'POSS')
-					sent[k+1] = (sent[k+1][0], 'POSS')
-				else:
-					sent[k] = (sent[k][0], 'POSS')
-					sent[k+1] = (sent[k+1][0], 'POSS')
-					if (k+2 < len(sent)): #adding if condition just to be on safe side
-						sent[k+2] = (sent[k+2][0], 'POSS') # so this doesn't go out of range
-						#when the sentence ends with NN's like John’s. Mary’s. Teachers’.
-				k-=1
+	while k<len(spacyml):
+		# print(spacyml[k], end='')
+		# print("\t\t", end='')
+		# if (k<len(sent)):
+		# 	print(sent[k])
 
-		elif (k!=0 and k!=len(sent)-1 # so that it doesn't raise an error in the coming lines
-			and
-			(sent[k-1][0][0] >= 'A' and sent[k-1][0][0] <= 'Z' and "NN" in sent[k-1][1]) #if the word before it, is a proper noun
-			and
-			("and" in sent[k][0] or "of" in sent[k][0]) # if it is a conjuction word in the name of a proper noun, like "Reserve Bank 'of' India"
-			and
-			(sent[k+1][0][0] >= 'A' and sent[k+1][0][0] <= 'Z' and "NN" in sent[k+1][1]) # if the next word is also a proper noun
-			):
-			nstr = nstr + sent[k][0] + " "
 
-		else: #something other than NN encountered 
-			if (len(nstr)>0): # if there is a NN to write 
-				nstr = nstr.strip()
-				nn = (nstr,) + ("NN",)
-				ml.append(nn) #write the NN
-				nstr = "" # clear the string
-				ml.append(sent[k]) # add the other-than-NN word
-			else:
-				ml.append(sent[k]) #just add it
+		if (spacyml[k][1]=='O' or str(spacyml[k][0]) == "'s"):
+			if (len(nstr)>0):
+				nn = (nstr.strip(),) + ("NN",)
+				newml.append(nn)
+				nstr = ""
+			newml.append((str(sent[k][0]),) + (str(sent[k][1]),))
+		else:
+			nstr = nstr + str(sent[k][0]) + " "
+
+
+
 		k+=1
-		if (k == len(sent)): #in case the last word was a noun in a sentence
-			nstr = nstr.strip()
-			nn = (nstr,) + ("NN",)
-			ml.append(nn)
-			nstr = ""
+
+	ml = newml
+	newml = []
+	nstr= ""
+	k=0
+	while k<len(ml):
+		if ( re.search(r'\d', ml[k][0]) ):
+			ml[k] = (ml[k][0], "CD")
+			if (len(nstr)>0):
+				nn = (nstr.strip(),) + ("NN",)
+				newml.append(nn)
+				nstr = ""
+			newml.append(ml[k])
+		elif (k+1 < len(ml) and ml[k+1][1] == "POS"):
+			ml[k] = (ml[k][0], "POS")
+			if (len(nstr)>0):
+				nn = (nstr.strip(),) + ("NN",)
+				newml.append(nn)
+				nstr = ""
+			newml.append(ml[k])
+		elif ("NN" in ml[k][1]):
+			nstr = nstr + ml[k][0] + " "
+		else:
+			if (len(nstr)>0):
+				nn = (nstr.strip(),) + ("NN",)
+				newml.append(nn)
+				nstr = ""
+			newml.append(ml[k])
+		k+=1
+
+	ml = newml
+	newml = []
 
 
+	print("NER MODIFIED")
+	print(nltk.ne_chunk(ml))
+	for y in ml:
+		print(y)
 
 
-	# print("NER MODIFIED")
-	# print(nltk.ne_chunk(ml))
-
+	input()
 
 	ignore_verbs = ["is","was","were","will","shall","must","should","would","can","could","may","might"] #verbs which are often modal verbs
 	entities = []
@@ -227,7 +251,7 @@ def getTriplets(ex):
 	entities = []
 	# here we select the possessions 
 	while k<len(ml):
-		if ("POSS" in ml[k][1]):
+		if ("POS" in ml[k][1]):
 
 			# if (k+3 >= len(ml)):
 			# 	print(ml[k-2], ml[k-1], ml[k], ml[k+1], ml[k+2])
@@ -237,15 +261,15 @@ def getTriplets(ex):
 			# 	input()
 
 			n1 = ml[k][0].replace('\'s','').replace('’s','')
-			if ("POSS" in ml[k+1][1]):
+			if (k+1<len(ml) and "POS" in ml[k+1][1]):
 				# n1 = n1+ml[k+1][0]
 				k+=1
-			if ("POSS" in ml[k+1][1]):
+			if (k+1<len(ml) and "POS" in ml[k+1][1]):
 				# n1 = n1+ml[k+1][0]
 				k+=1
 			r = "belongs-to"
 
-			if ("NN" in ml[k+1][1]):
+			if (k+1<len(ml) and "NN" in ml[k+1][1]):
 				n2 = ml[k+1][0]
 				triplets.append((n2,)+(r,)+(n1,)) #order of n1 and n2 changed intentionally
 		k+=1
@@ -268,10 +292,10 @@ def getTriplets(ex):
 	# 		k-=1
 	# 	k+=1
 
-	## to print triplets
-	# print("Triplets modified")
-	# for x in triplets:
-	# 	print(x)
+	# to print triplets
+	print("Triplets modified")
+	for x in triplets:
+		print(x)
 
 	return triplets
 
@@ -305,12 +329,13 @@ with open('NEW_New_Coref_Dataset.csv', 'r') as csvFile:
 		# input()
 		# continue
 		article = row[1]
+		article = "John Sowa Mayar\'s lawyer appealed to International Museum of Trade and Commerce to hear the case of Nirav Modi, Mehul Choksy and Ramanujam. Enlarge ImageBetween this and the Venue, \"Hyundai Motor Group\" is on a visual roll right now. What's the best way to market a car to millennials? By making one that's small enough for them to actually afford, duh. Kia on Tuesday unveiled the first sketches of its upcoming small SUV. We won't see the full thing until the summer, but for now, the sketches give us an idea of what Kia will bring to market in an effort to woo urbanite millennials into car ownership. The car will allegedly be a global SUV, meaning it's destined for a whole bunch of markets, but considering Autocar's report claims it won't be coming to Europe, it's uncertain if the US is involved in this rollout. It's also unclear if Kia actually understands what \"global\" means. From the design side, Kia apparently took a boatload of inspiration from its SP Signature Concept, which debuted at the Seoul Motor Show in March."
 		article = clearBrackets(article)
 		triplets = []
 		for x in sent_tokenize(article):
 			ml = getTriplets(x) #getting triplets for each sentence
 			triplets+=ml
-			# input() #to pause execution
+			input() #to pause execution
 
 
 		#at this point triplets variable contains all the triples from the article
@@ -327,14 +352,14 @@ with open('NEW_New_Coref_Dataset.csv', 'r') as csvFile:
 
 		k+=1
 		print(str(k)+" / "+str(tlen))
-		# if k>2: #just to see output from top 2 articles
-		# 	break
+		if k>2: #just to see output from top 2 articles
+			break
 
-## to write into a file
-file = open('new_5.csv','w')
-for x in output:
-	for y in x:
-		file.write(y.replace(',','').replace('‘','\'').replace('’','\'').replace('“','\'').replace('”','\'')+', ')
-	file.write("\n")
-file.close()
+### to write into a file
+# file = open('new_5.csv','w')
+# for x in output:
+# 	for y in x:
+# 		file.write(y.replace(',','').replace('‘','\'').replace('’','\'').replace('“','\'').replace('”','\'')+', ')
+# 	file.write("\n")
+# file.close()
 csvFile.close()
